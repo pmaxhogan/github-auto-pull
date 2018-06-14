2eecd109c9818d6f19bc309b59d7610af39879bf
programmer5000-com <programmer5000@programmer5000.com>
2018-06-14 12:17:23 -0400

    Add command-line application stuff

--- index.js
+++ index.js
@@ -1,41 +1,102 @@
+const fs = require("fs");
+const path = require("path");
 const crypto = require("crypto");
 const http = require("http");
+const { spawn } = require("child_process");
 const port = 80;
+
+if(process.argv.length !== 3 && process.argv.length !== 4){
+	console.log(`
+github-auto-pull [fileToRun] <secret>
+
+Creates a github webhook server running on port 80.
+`);
+	process.exit();
+}
+
+const fileToRun = process.argv[2];
+const secret = process.argv[3];
+if(secret){
+	console.log("running", fileToRun, "when a correct push with the secret", secret);
+}else{
+	console.log("running", fileToRun, "when a push is sent. You should configure a secret in your webhooks because this prevents somebody from DoSing you by continually sending fake pushes.");
+}
+
+const logfile = path.join(path.dirname(fileToRun), "out.log");
+console.log("Program output will be logged to " + logfile);
+
+const out = fs.openSync(logfile, "a");
+const err = fs.openSync(logfile, "a");
+
+const procBody = (body) => {
+	const json = JSON.parse(body);
+	if(json.zen){
+		console.log(`Got ping from ${json.repository.html_url} (sender: ${json.sender.login})`);
+	}else if(json.pusher){
+		console.log(`Got commits from ${json.pusher.name}! ${json.commits.reduce((str, commit) => str + `\n${commit.id} ${commit.author.name}: ${commit.message}`, "")}`);
+
+		const procError = err => {
+			if(err.code === "EACCES"){
+				console.error(`It looks like you've forgot to give yourself execute permissions on your script.
+Run
+chmod +x ${fileToRun}`);
+			}else{
+				console.error("Could not spawn", fileToRun, "got error", err);
+			}
+		};
+
+		try{
+			const subprocess = spawn(fileToRun, [], {
+				detached: true,
+				stdio: ["ignore", out, err]
+			});
+
+			subprocess.on("close", (code) => {
+				if (code !== 0) {
+					console.log(`Script exited with code ${code}`);
+				}
+			});
+
+			subprocess.on("error", procError);
+
+			subprocess.unref();
+		}catch (e){
+			procError(e);
+		}
+	}
+};
 
 const requestHandler = (request, response) => {
 	let body = [];
+	let hmac;
 
-	const hmac = crypto.createHmac("sha1", "OHHAI");
+	if(secret) hmac = crypto.createHmac("sha1", secret);
 
 	request.on("data", (chunk) => {
 		body.push(chunk);
 	}).on("end", () => {
 		body = Buffer.concat(body).toString();
 
-		if(!body || !body.toString()) response.end("hi");
+		if(!body || !body.toString()) response.end("No message was sent.");
 
-		hmac.on("readable", () => {
-			const data = hmac.read();
-			if (data) {
-				const hash = data.toString("hex");
-				const matches = "sha1=" + hash === request.headers["x-hub-signature"];
-				if(matches){
-					const json = JSON.parse(body);
-					if(json.zen){
-						console.log(`Got ping from ${json.repository.html_url} (sender: ${json.sender.login})`);
-					}else if(json.pusher){
-						console.log(`Got commits from ${json.pusher.name}! ${json.commits.reduce((str, commit) => str + `
-${commit.id} ${commit.author.name}: ${commit.message}`)}`);
+		if(secret){
+			hmac.on("readable", () => {
+				const data = hmac.read();
+				if (data) {
+					const hash = data.toString("hex");
+					const matches = "sha1=" + hash === request.headers["x-hub-signature"];
+					if(matches){
+						procBody(body);
+					}else{
+						console.error(`HMAC didn't match! Calculated sha1=${hash}, was sent ${request.headers["x-hub-signature"]}`);
 					}
-				}else{
-					console.error(`HMAC didn't match! Calculated sha1=${hash}, was sent ${request.headers["x-hub-signature"]}`);
 				}
-			}
-		});
-
-
-		hmac.write(body);
-		hmac.end();
+			});
+		}
+		if(secret){
+			hmac.write(body);
+			hmac.end();
+		}
 		response.end("hi");
 	});
 };
